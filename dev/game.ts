1ed21d237d01246102b75d475c075f67889fce62
stefandegier <stefan_de_gier@outlook.com>
2021-06-09 11:07:45 +0200

    Added init game class

--- dev/game.ts
+++ dev/game.ts
@@ -1,1 +1,16 @@
-class Game {}
+class Game {
+    constructor() {
+        console.log("Game was created!")
+        console.log("hey?")
+        // create board
+        this.gameLoop()
+    }
+
+    gameLoop() {
+        //TODO: update board
+        
+        requestAnimationFrame(() => this.gameLoop())
+    }
+}
+
+new Game()
