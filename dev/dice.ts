8ce0c12a3b482743a81f86edb0d882eee82205db
unknown <stefan_de_gier@outlook.com>
2021-06-15 22:49:14 +0200

    Added dice

---
+++ dev/dice.ts
@@ -0,0 +1,30 @@
+// import { Game } from "./game.js"
+
+export class Dice {
+
+    constructor() {  
+        console.log("dice");
+    }
+
+    public rollDice() : Number {
+        const dice = [...document.querySelectorAll(".die-list")];
+        let diceValue = this.getRandomNumber(1, 6);
+        dice.forEach((die) => {
+            this.toggleClasses(die)
+            die.dataset.roll = diceValue;
+        });
+
+        return diceValue;
+    }
+      
+    private toggleClasses(die: Element) : void {
+        die.classList.toggle("odd-roll");
+        die.classList.toggle("even-roll");
+    }
+    
+    private getRandomNumber(min: number, max: number) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+}
