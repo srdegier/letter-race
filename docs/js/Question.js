5081fc888fece7604e1bf6172d64eb9cf27b3d91
Sander <sanderjverhoeven@hotmail.com>
2021-06-18 15:00:45 +0200

    Added new font and styled tiles

--- docs/js/Question.js
+++ docs/js/Question.js
@@ -1,9 +1,9 @@
 export class Question {
-    constructor() {
+    constructor(player) {
         this.div = [];
         this.questions = [
             {
-                "image": "",
+                "image": "./images/boom.png",
                 "solution": "oo",
                 "question": "bm",
                 "wholeAnswer": "boom",
@@ -14,7 +14,7 @@ export class Question {
                 ]
             },
             {
-                "image": "",
+                "image": "./images/maan.png",
                 "solution": "aa",
                 "question": "mn",
                 "wholeAnswer": "maan",
@@ -25,6 +25,7 @@ export class Question {
                 ]
             }
         ];
+        this.player = player;
         console.log('question');
         document.querySelectorAll('.answer').forEach(item => {
             item.addEventListener('click', event => {
@@ -32,17 +33,56 @@ export class Question {
             });
         });
     }
+    timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
     create() {
         var _a;
-        let random = Math.floor(Math.random() * 2);
-        let question = this.questions[random];
+        this.cleanModal();
+        this.chosenQ = Math.floor(Math.random() * 2);
+        let question = this.questions[this.chosenQ];
         var showQuestion = question.wholeAnswer.replace(question.solution, '?');
         (_a = document.getElementById("question")) === null || _a === void 0 ? void 0 : _a.innerText = showQuestion;
+        document.getElementById("question-image").src = question.image;
         document.getElementById("myModal").style.display = "block";
     }
+    cleanModal() {
+        const modal = document.getElementById('modal');
+        modal === null || modal === void 0 ? void 0 : modal.classList.remove('correct');
+        modal === null || modal === void 0 ? void 0 : modal.classList.remove('incorrect');
+        const questionText = document.getElementById('question');
+        questionText === null || questionText === void 0 ? void 0 : questionText.classList.remove('visible');
+        const message = document.getElementById('message');
+        message === null || message === void 0 ? void 0 : message.classList.remove('visible');
+        const answerDivs = [...document.querySelectorAll(".answer")];
+        answerDivs.forEach((answerDiv) => {
+            answerDiv.classList.remove('invisible');
+        });
+    }
     checkAnswer(e) {
-        console.log(e.target.innerText);
-        console.log(this.questions);
+        let givenAnswer = e.target.innerText;
+        const question = this.questions[this.chosenQ];
+        const answerDivs = [...document.querySelectorAll(".answer")];
+        answerDivs.forEach((answerDiv) => {
+            answerDiv.classList.add('invisible');
+        });
+        const message = document.getElementById('message');
+        message.className = 'visible';
+        const modal = document.getElementById('modal');
+        const messageStatus = question.wholeAnswer.replace(question.solution, givenAnswer.toLowerCase());
+        const questionText = document.getElementById('question');
+        questionText === null || questionText === void 0 ? void 0 : questionText.innerText = messageStatus;
+        if (givenAnswer.toLowerCase() == question.solution) {
+            message === null || message === void 0 ? void 0 : message.innerText = `Het is een ${messageStatus}!`;
+            modal === null || modal === void 0 ? void 0 : modal.classList.add('correct');
+        }
+        else {
+            modal === null || modal === void 0 ? void 0 : modal.classList.add('incorrect');
+            message === null || message === void 0 ? void 0 : message.innerText = `Het is geen ${messageStatus}!`;
+        }
+        this.timeout(4000).then(() => {
+            document.getElementById("myModal").style.display = "none";
+        });
     }
 }
-//# sourceMappingURL=Question.js.map
+//# sourceMappingURL=question.js.map
