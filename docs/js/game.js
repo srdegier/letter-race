40bb8001fab7430e0d4191a50d034e8141deff5c
TJ <71647978+TJ-CMGT@users.noreply.github.com>
2021-06-22 19:43:03 +0200

    Fixed game theme sound

--- docs/js/game.js
+++ docs/js/game.js
@@ -24,8 +24,6 @@ class Game {
         this.timeout(2000).then(() => {
             console.log('Player dice');
             this.player[pTurn].move(diceValue, false);
-            const victorySound = new Audio('./sounds/safari_theme.mp3');
-            victorySound.play();
         });
     }
 }
--- docs/js/themeSounds.js
+++ docs/js/themeSounds.js
@@ -1,19 +1,23 @@
-// Checks if script is running
+// Plays main game theme sound if game is loaded
 window.addEventListener('load', (event) => {
-    console.log('The music theme is loaded');
-});
-// Plays sound if player hovers or stops hovering over either the image or the button. Note: only works if user has click the image due to Chrome limitations 
-const victorySound = new Audio('./sounds/safari_theme.mp3');
-
-addEventListener("mouseover", playTheme, victorySound);
-addEventListener("mouseout", stopTheme, victorySound);
-
-function playTheme() {
+    console.log('Main game theme sound is loaded!');
+    const victorySound = new Audio('./sounds/safari_theme.mp3');
     victorySound.play();
-    console.log("Music theme activated!");
-}
+    victorySound.loop = true;
+});
 
-function stopTheme() {
-    victorySound.play
-    console.log("Music theme activated!");
-}   
+// Plays starting screen game theme sound if player hovers or stops hovering over either the image or the button. Note: only works if user has click the image due to Chrome limitations 
+// const victorySound = new Audio('./sounds/safari_theme.mp3');
+
+// addEventListener("mouseover", playTheme, victorySound);
+// addEventListener("mouseout", stopTheme, victorySound);
+
+// function playTheme() {
+//     victorySound.play();
+//     console.log("Music theme activated!");
+// }
+
+// function stopTheme() {
+//     victorySound.play
+//     console.log("Music theme activated!");
+// }   
